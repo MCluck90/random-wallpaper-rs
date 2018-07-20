1d3f83fb67f6fa7d8ae41e4bd5d267382b072588
Mike Cluck <mcluck90@gmail.com>
2018-07-20 10:16:28 -0400

    Added a bunch of error handling

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,30 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 fn main() {
+  if let Err(e) = run() {
+    println!("{}", e);
+    std::process::exit(1);
+  }
+}
+
+fn run() -> Result<(), String> {
   // Known image extensions
   let image_extensions = vec!["bmp", "gif", "jpg", "jpeg", "png"];
 
   // Get the current wallpaper
-  let current_wallpaper = wallpaper::get().unwrap();
+  let current_wallpaper = wallpaper::get().map_err(|err| err.to_string())?;
   let current_wallpaper_path = Path::new(&current_wallpaper);
-  let current_wallpaper = current_wallpaper_path.to_str().unwrap();
+  let current_wallpaper = current_wallpaper_path
+    .to_str()
+    .ok_or("Path to current wallpaper is not valid Unicode")?;
 
   // Find the rest of the wallpapers in the same directory
-  let wallpaper_dir_path = current_wallpaper_path.parent().unwrap();
+  let wallpaper_dir_path = current_wallpaper_path
+    .parent()
+    .ok_or("Failed to get image directory. Is your wallpaper the actual root of your system?")?;
+
   let wallpapers = fs::read_dir(wallpaper_dir_path)
-    .unwrap()
+    .map_err(|err| err.to_string())?
     .map(|w| w.unwrap().path())
     // Filter out folders
     .filter(|w| w.is_file())
@@ -33,11 +45,11 @@ fn main() {
 
   // Make sure at least one file could be found
   if wallpapers.len() == 0 {
-    println!(
+    return Err(
       "It appears as though there's only one image in the same folder as \
        your current wallpaper. Could not find a new wallpaper to switch to."
+        .to_string(),
     );
-    std::process::exit(1);
   }
 
   // Pick a random wallpaper
@@ -49,5 +61,7 @@ fn main() {
   println!("{}", new_wallpaper.display());
 
   // Assign it as the new wallpaper
-  wallpaper::set_from_file(new_wallpaper.to_str().unwrap()).unwrap();
+  wallpaper::set_from_file(new_wallpaper.to_str().unwrap()).map_err(|err| err.to_string())?;
+
+  Ok(())
 }
