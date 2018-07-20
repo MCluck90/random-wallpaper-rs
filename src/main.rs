c684a3a33e8b3eb0a466a00f3c06f833de2c4153
Mike Cluck <mcluck90@gmail.com>
2018-07-20 09:14:46 -0400

    Lists out all possible wallpapers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 extern crate wallpaper;
 
+use std::fs;
+use std::path::Path;
+
 fn main() {
-  println!("{:?}", wallpaper::get());
+  let current_wallpaper = wallpaper::get().unwrap();
+  let current_wallpaper_path = Path::new(&current_wallpaper);
+  let wallpaper_dir_path = current_wallpaper_path.parent().unwrap();
+  let wallpapers = fs::read_dir(wallpaper_dir_path)
+    .unwrap()
+    .map(|w| w.unwrap().path())
+    .filter(|w| w.is_file());
+
+  for w in wallpapers {
+    println!("{}", w.display());
+  }
 }
